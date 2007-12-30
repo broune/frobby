91eaa5c2d364a6bddc0a8d036f59b25e18e249fa
Bjarke Hammersholt Roune <broune@users.noreply.github.com>
2007-12-30 07:57:33 +0100

    Reorganized IO code.

--- src/io.h
+++ src/io.h
@@ -3,94 +3,34 @@
 
 #include "TermConsumer.h"
 #include "VarNames.h"
-#include "TermTranslator.h"
-#include "Term.h"
 
 #include <vector>
 
+class Term;
 class Lexer;
 class BigIdeal;
-class VarNames;
+class TermTranslator;
 
 class IdealWriter : public TermConsumer {
  public:
-  IdealWriter() {}
-  IdealWriter(FILE* file, const VarNames& names):
-    _file(file),
-    _names(names),
-    _translator(0) {
-  }
-  IdealWriter(FILE* file, const TermTranslator* translator):
-    _file(file),
-    _names(translator->getNames()),
-    _translator(translator) {
-    _translator->makeStrings();
-  }
-  virtual ~IdealWriter() {};
+  IdealWriter();
+  IdealWriter(FILE* file, const VarNames& names);
+  IdealWriter(FILE* file, const TermTranslator* translator);
+  virtual ~IdealWriter();
 
-  virtual void consume(const vector<const char*>& term) = 0;
   virtual void consume(const vector<mpz_class>& term) = 0;
   virtual void consume(const Term& term) = 0;
 
  protected:
-  static void writeTerm(const vector<const char*>& term, FILE* file) {
-    char separator = ' ';
-    size_t varCount = term.size();
-    for (size_t j = 0; j < varCount; ++j) {
-      const char* exp = term[j];
-      if (exp == 0)
-	continue;
-
-      putc(separator, file);
-      separator = '*';
-
-      fputs(exp, file);
-    }
-
-    if (separator == ' ')
-      fputs(" 1", file);
-  }
+  static void writeTerm(const vector<const char*>& term, FILE* file);
 
   static void writeTerm(const Term& term,
 			const TermTranslator* translator,
-			FILE* file) {
-    char separator = ' ';
-    size_t varCount = term.getVarCount();
-    for (size_t j = 0; j < varCount; ++j) {
-      const char* exp = translator->getExponentString(j, term[j]);
-      if (exp == 0)
-	continue;
-
-      putc(separator, file);
-      separator = '*';
-
-      fputs(exp, file);
-    }
-
-    if (separator == ' ')
-      fputs(" 1", file);
-  }
+			FILE* file);
 
   static void writeTerm(const vector<mpz_class>& term,
 			const VarNames& names,
-			FILE* file) {
-    char separator = ' ';
-    size_t varCount = term.size();
-    for (size_t j = 0; j < varCount; ++j) {
-      if ((term[j]) == 0)
-	continue;
-
-      putc(separator, file);
-      separator = '*';
-
-      fputs(names.getName(j).c_str(), file);
-      if ((term[j]) != 1)
-	gmp_printf("^%Zd", term[j].get_mpz_t());
-    }
-
-    if (separator == ' ')
-      fputs(" 1", file);
-  }
+			FILE* file);
 
   FILE* _file;
   VarNames _names;
