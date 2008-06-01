21f6d726c546bba15accf3a09fa543e10e7778bb
Bjarke Hammersholt Roune (www.broune.com) <Bjarke Hammersholt Roune (www.broune.com)@localhost>
2008-06-01 02:05:39 +0200

    Added missing files from last commit.

---
+++ src/CoCoA4IOHandler.h
@@ -0,0 +1,51 @@
+/* Frobby: Software for monomial ideal computations.
+   Copyright (C) 2007 Bjarke Hammersholt Roune (www.broune.com)
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see http://www.gnu.org/licenses/.
+*/
+#ifndef COCOA_4_IO_HANDLER_GUARD
+#define COCOA_4_IO_HANDLER_GUARD
+
+#include "IOHandler.h"
+
+class VarNames;
+class Scanner;
+class BigIdeal;
+class CoefTermConsumer;
+
+class CoCoA4IOHandler : public IOHandler {
+public:
+  CoCoA4IOHandler();
+
+  virtual void readIdeal(Scanner& scanner, BigIdeal& ideal);
+
+  virtual void writeIdealHeader(const VarNames& names, FILE* out);
+  virtual void writeTermOfIdeal(const Term& term,
+								const TermTranslator* translator,
+								bool isFirst,
+								FILE* out);
+  virtual void writeTermOfIdeal(const vector<mpz_class> term,
+								const VarNames& names,
+								bool isFirst,
+								FILE* out);
+  virtual void writeIdealFooter(FILE* out);
+
+  virtual CoefTermConsumer* createCoefTermWriter
+	(FILE* file, const TermTranslator* translator);
+
+private:
+  void readVarsAndClearIdeal(BigIdeal& ideal, Scanner& scanner);
+};
+
+#endif
