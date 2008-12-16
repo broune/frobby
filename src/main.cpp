8e995e0ef2baf6a075a0e42af2e9d817d95f002f
Bjarke Hammersholt Roune (www.broune.com) <Bjarke Hammersholt Roune (www.broune.com)@localhost>
2008-12-16 17:18:41 -0500

    Cleaned up and improved allocation debugging code.

--- src/main.cpp
+++ src/main.cpp
@@ -15,129 +15,26 @@
    along with this program.  If not, see http://www.gnu.org/licenses/.
 */
 #include "stdinc.h"
+#include "main.h"
 
 #include "Action.h"
+#include "DebugAllocator.h"
 #include "error.h"
 
-#ifdef DEBUG
+int frobbyMain(int argc, const char** argv) {
+  string prefix;
+  if (argc > 1) {
+	prefix = argv[1];
+	--argc;
+	++argv;
+  } else
+	prefix = "help";
 
-bool detailAllocation = false;
-bool debuggingAllocation = false;
-size_t goodAllocationsLeft = 0;
+  const auto_ptr<Action> action(Action::createActionWithPrefix(prefix));
+  action->parseCommandLine(argc - 1, argv + 1);
+  action->perform();
 
-#undef new
-
-void* myOperatorNew(size_t s, bool array,
-					const char* file, size_t line) {
-  if (detailAllocation) {
-	if (file != 0)
-	  fprintf(stderr, "Allocating %i bytes at %s:%i.\n",
-			  (int)s, file, (int)line);
-	if (file == 0)
-	  fprintf(stderr, "Allocating %i bytes at an unknown point.\n",
-			  (int)s);
-  }
-
-  if (debuggingAllocation && goodAllocationsLeft == 0) {
-	if (detailAllocation)
-	  fputs("Throwing bad_alloc due to artifically imposed limit.\n", stderr);
-	throw bad_alloc();
-  }
-
-  void* p;
-  if (array)
-	p = malloc(s);
-  else
-	p = malloc(s);
-  if (debuggingAllocation)
-	--goodAllocationsLeft;
-  return p;
-}
-
-void* operator new(size_t s) throw (std::bad_alloc) {
-  return myOperatorNew(s, false, 0, 0);
-}
-
-void* operator new[](size_t s) throw (std::bad_alloc) {
-  return myOperatorNew(s, true, 0, 0);
-}
-
-void operator delete(void* s) throw() {
-  free(s);
-}
-
-void operator delete[](void* s) throw() {
-  free(s);
-}
-
-void* operator new(size_t s, const char* file, size_t line) throw (std::bad_alloc) {
-  return myOperatorNew(s, false, file, line);
-}
-
-void* operator new[](size_t s, const char* file, size_t line) throw (std::bad_alloc) {
-  return myOperatorNew(s, true, file, line);
-}
-
-// Note that these overloads to operator delete are strictly
-// necessary, even though they will only be called in the very special
-// circumstance where an exception gets thrown while an object
-// allocated using the new above has been *partially* constructed.
-//
-// However, when that happens, this overload gets called. The compiler
-// will happily substitute its own automatically generated version,
-// which does *not* deallocate the memory, leading to a memory leak.
-//
-// It is also important that these overloads are declared in each
-// translation unit, as otherwise the compiler generated do-nothing
-// version will be used in those translation units that have no such
-// declaration. To me this all falls into the category of "Insane but
-// true".
-void operator delete(void* s, const char* file, size_t line) {
-  operator delete(s);
-}
-
-void operator delete[](void* s, const char* file, size_t line) {
-  operator delete[](s);
-}
-
-#endif
-
-int normalMain(int argc, const char** argv) {
-  try {
-	srand((unsigned int)time(0) +
-		  (unsigned int)getpid() +
-		  (unsigned int)clock());
-
-	string prefix;
-	if (argc > 1) {
-	  prefix = argv[1];
-	  --argc;
-	  ++argv;
-	} else
-	  prefix = "help";
-
-	const auto_ptr<Action> action(Action::createActionWithPrefix(prefix));
-	action->parseCommandLine(argc - 1, argv + 1);
-	action->perform();
-
-	return 0;
-  } catch (const bad_alloc& e) {
-#ifdef DEBUG
-	if (debuggingAllocation && goodAllocationsLeft == 0)
-	  throw;
-#endif
-	reportErrorNoThrow("Ran out of memory.");
-	return 1;
-  } catch (const InternalFrobbyException& e) {
-	reportErrorNoThrow(e);
-	return 2;
-  } catch (const FrobbyException& e) {
-	reportErrorNoThrow(e);
-	return 1;
-  } catch (...) {
-	reportErrorNoThrow("An unexpected error occured.");
-	return 1;
-  }
+  return ExitCodeSuccess;
 }
 
 // A replacement for the default C++ built-in terminate() function. Do
@@ -161,97 +58,41 @@ void frobbyUnexpected() {
 }
 
 int main(int argc, const char** argv) {
-  set_terminate(frobbyTerminate);
-  set_unexpected(frobbyUnexpected);
+  try {
+	set_terminate(frobbyTerminate);
+	set_unexpected(frobbyUnexpected);
 
-#ifdef DEBUG
-  fputs("This is a DEBUG build of Frobby. It is therefore SLOW.\n", stderr);
-  fflush(stderr);
-#endif
+	srand((unsigned int)time(0) +
+		  (unsigned int)getpid() +
+		  (unsigned int)clock());
+
 #ifdef PROFILE
-  fputs("This is a PROFILE build of Frobby. It is therefore SLOW.\n", stderr);
-  fflush(stderr);
+	fputs("This is a PROFILE build of Frobby. It is therefore SLOW.\n",
+		  stderr);
 #endif
+#ifdef DEBUG
+	fputs("This is a DEBUG build of Frobby. It is therefore SLOW.\n",
+		  stderr);
+#endif
+
+
 
 #ifdef DEBUG
-  bool optionDebugAlloc = false;
-  bool optionDetailAlloc = false;
-  bool optionFileInput = false;
-  string inputFile;
-
-  const char* originalArgvZero = argv[0];
-  while (true) {
-	if (argc <= 1 || argv[1][0] != '_')
-	  break;
-
-	string option(argv[1] + 1);
-	argv += 1;
-	argc -= 1;
-
-	if (option == "debug-alloc")
-	  optionDebugAlloc = true;
-	else if (option == "detail-alloc")
-	  optionDetailAlloc = true;
-	else if (option == "input") {
-	  optionFileInput = true;
-	  if (argc <= 1) {
-		reportErrorNoThrow("Debug option _input requries an argument.");
-		return 1;
-	  }
-	  inputFile = argv[1];
-	  argv += 1;
-	  argc -= 1;
-	} else {
-	  reportErrorNoThrow("Unknown debug option \"" + option + "\".");
-	  return 1;
-	}
-  }
-  argv[0] = originalArgvZero;
-
-  detailAllocation = optionDetailAlloc;
-
-  if (optionDebugAlloc) {
-	fclose(stdout);
-
-	for (size_t goodAllocations = 0; true; ++goodAllocations) {
-	  if (goodAllocations % 100 == 0)
-		fprintf(stderr, "DEBUG: Trying allocation limits %i-%i.\n",
-				(int)goodAllocations, (int)(goodAllocations + 99));
-
-	  if (optionFileInput && !freopen(inputFile.c_str(), "r", stdin)) {
-		reportErrorNoThrow
-		  ("Could not open file \"" + inputFile + "\" for input.");
-		return 1;
-	  }
-
-	  debuggingAllocation = true;
-	  detailAllocation = optionDetailAlloc;
-	  goodAllocationsLeft = goodAllocations;
-
-	  try {
-		int exitValue = normalMain(argc, argv);
-		detailAllocation = optionDetailAlloc;
-		debuggingAllocation = false;
-		fprintf(stderr,
-				"DEBUG: normal return from main with exit value of %i.",
-				(int)exitValue);
-		return exitValue;
-	  } catch (bad_alloc) {
-		detailAllocation = false;
-		debuggingAllocation = false;
-		continue;
-	  }
-	}
-  }
-
-  if (optionFileInput) {
-	if (optionFileInput && !freopen(inputFile.c_str(), "r", stdin)) {
-	  reportErrorNoThrow
-		("Could not open file \"" + inputFile + "\" for input.");
-	  return 1;
-	}
-  }
+	return DebugAllocator::getSingleton().runDebugMain(argc, argv);
+#else
+	return frobbyMain(argc, argv);
 #endif
-
-  return normalMain(argc, argv);
+  } catch (const bad_alloc& e) {
+	reportErrorNoThrow("Ran out of memory.");
+	return ExitCodeOutOfMemory;
+  } catch (const InternalFrobbyException& e) {
+	reportErrorNoThrow(e);
+	return ExitCodeInternalError;
+  } catch (const FrobbyException& e) {
+	reportErrorNoThrow(e);
+	return ExitCodeError;
+  } catch (...) {
+	reportErrorNoThrow("An unexpected error occured.");
+	return ExitCodeUnknownError;
+  }
 }
