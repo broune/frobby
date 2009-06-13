dcaa83b02d06514374fa7bcd9ff0e644a1fb1efa
Bjarke Hammersholt Roune (www.broune.com) <Bjarke Hammersholt Roune (www.broune.com)@localhost>
2009-06-13 03:33:11 +0200

    Fixed a bug for OptimizationStrategy and heavily extended the comments for that class also.

--- src/OptimizeStrategy.h
+++ src/OptimizeStrategy.h
@@ -42,14 +42,27 @@ class TermGrader;
 */
 class OptimizeStrategy : public MsmStrategy, public TermConsumer {
 public:
+  /** Construct an OptimizeStrategy.
+
+   @param grader This object assigns values to monomials.
+
+   @param splitStrategy The split selection strategy to use.
+
+   @param reportAllSolutions Compute all msm's of optimal value if
+   true. Otherwise compute a single msm of optimal value.
+
+   @param useBound If true, make use of the branch-and-bound
+    optimization based on eliminating non-improving slices.
+  */
   OptimizeStrategy(TermGrader& grader,
 				   const SplitStrategy* splitStrategy,
 				   bool reportAllSolutions,
 				   bool useBound);
 
+  /** Returns the msm's with optimal value found so far. */
   const Ideal& getMaximalSolutions();
 
-  /** The optimal value associated to each entry from
+  /** The optimal value associated to all entries from
    getMaximalSolutions(). This method can only be called if
    getMaximalSolutions() is not the zero ideal.
   */
@@ -61,6 +74,7 @@ public:
   virtual void setUseIndependence(bool use);
 
   virtual void getPivot(Term& pivot, Slice& slice);
+
   virtual void simplify(Slice& slice);
 
   virtual void beginConsuming();
@@ -68,45 +82,208 @@ public:
   virtual void doneConsuming();
 
  private:
-  /** We are considering a slice where each msm d in the content
-    fulfills that lowerBound divides d and d divides
-    upperBound. upperBoundDegree is the degree of upperBound and is
-    passed as a parameter so that it does not have to be recomputed.
+  /** This method simplifies a slice based on generating non-improving
+   outer and inner slices. The idea is conceptually simple, but it has
+   been quite a challenge to get the code to be correct, efficient and
+   as good as possible in terms of simplifying as much as
+   possible. This is why there are so many tests related to this, and
+   why the comments for this and related methods are so detailed.
 
-	Suppose we perform a pivot split on var^e for some e. Then the
-	outer slice will have a monomial upper bound whose exponent of var
-	is at most e. So if the degree of upperBound with the exponent of
-	var replaced by e is worse than the best value found so far, then
-	this outer slice will be non-promising. Thus we only have to
-	consider the inner slice, which will have lowerBound increased by
-	var^e. This method returns the largest value of e for which this
-	works.
+   To each slice we associate a divisor \f$x^a\f$ and a dominator
+   \f$x^b\f$, which respectively divide and dominate each msm \f$m\f$
+   in the content of the slice. I.e. \f$x^a|m|x^b\f$. We summarize
+   this as a pair \f$(a,b)\f$. Let \f$d(a,b)\f$ be the associated
+   upper bound, i.e. the largest possible value that the grader
+   associates to a monomial \f$x^v\f$ such that \f$a\leq v\leq
+   b\f$. Thus \f$d(a,b)\f$ is an uppper bound on the value of any msm
+   in the content of the slice to which \f$(a,b)\f$ is associated.
 
-	TODO: update.
-   */
+   To be more exact, for a slice \f$(I,S,q)\f$, \f$q\f$ is the
+   divisor, while \f$q\textrm{lcm}(\min(I)):x_1\cdots x_n\f$ is the
+   dominator. Also, let \f$d_i(t)\f$ be the value of \f$x_i^t\f$ and
+   let \f$\max\f$ be the highest value according to the grader of an
+   msm found so far.
+
+   This allows us to discard slices that are non-improving, in that
+   they do not improve on the best value found so far. Note that
+   whether "improve" means "strictly improve" or "weakly improve"
+   depends on whether we are reporting all optimal msm's or just
+   one. In the following discussion we will assume that we are looking
+   for just one msm, so that a slice is non-improving if it does not
+   strictly improve.
+
+   This method is called when the slice itself cannot be seen to be
+   non-improving from the divisor and dominator. However, if we
+   consider pivot splits on pivots that are pure powers, i.e. of the
+   form \f$x_i^t\f$, then we can compute a divisor and dominator for
+   both the inner and outer slice just from the divisor and dominator
+   of the current slice. Note that the divisor and dominator assigned
+   in this way are not necessarily as tight as the ones we would get
+   if we actually computed the inner and outer slice.
+
+   It may be that we can see that the inner or outer slice from above
+   is non-improving. If the inner slice is non-improving, then we can
+   discard it and replace the current slice with the outer slice, and
+   vice versa if the outer slice is non-improving. This is
+   bound-driven simplification of a slice, and it is efficient since
+   we can perform these calculations looking only at the two monomials
+   divisor and dominator.
+
+   Consider a pivot split on \f$x_i^t\f$. Then the outer slice has
+   \f$(a, b^\prime)\f$, where \f$b^\prime\f$ is equal to \f$b\f$
+   except that \f$b_i^\prime:= a_i+t-1\f$. The inner slice will have
+   \f$(a',b)\f$, where \f$a^\prime\f$ is equal to \f$a\f$ except that
+   \f$a^\prime_i := a_i + t\f$. Note that the sensible \f$t\f$'s to
+   consider are those for which \f$a_i<a_i+t\leq b_i\f$. We need to
+   work in terms of \f$a_i+t\f$ rather than just \f$t\f$ since we work
+   with a TermGrader that has no reasonable way of making sense of
+   just t.
+
+   If possible, this method moves to an inner or outer slice of the
+   parameter slice based on this reasoning.
+
+   See getInnerSimplify and getOuterSimplify for more details. Which
+   one of those two is relevant for a given slice and variable depends
+   on the sign of that variable in the grading, and on whether the
+   ideal contains a pure power that maps to the same value as zero
+   does. We call the latter phenomenon a "fake power".
+
+   @param slice The slice to work on.
+
+   @param dominator The dominator of slice (passed as a parameter to
+    avoid recomputation).
+
+   @param degree The upper bound \f$d(a,b)\f$ associated to slice (to
+    avoid recomputation).
+
+   @return Returns true if and only if slice changed.
+  */
 
   bool boundSimplify
 	(Slice& slice,
 	 const Term& dominator,
 	 const mpz_class& degree);
 
+  /** Find an outer slice that is non-improving, allowing us to
+   replace the current slice with the inner slice. We only need to
+   know a divisor \f$a\f$ and dominator \f$b\f$ of the current slice to
+   perform this analysis. If \f$a_i=b_i\f$ then there is nothing to be
+   done for the \f$i\f$'th entry, so assume that \f$a_i<b_i\f$.
+
+   There are two ways to obtain a non-improving outer slice. The first
+   is if the grading sign is positive, and \f$d(a,b^\prime)\leq
+   \max\f$. Let \f$B:=b_i\f$ if \f$b_i\f$ is not a fake exponent, and let
+   \f$B := b_i-1\f$ if \f$b_i\f$ is a fake exponent. Then we have that \f[
+     d(a,b^\prime) = d(a,b) - d_i(B) + d_i(a_i+t-1),
+   \f] whereby \f$d(a,b^\prime)\leq \max\f$ is equivalent to \f[
+     d_i(a_i+t-1) \leq \max - d(a,b) + d_i(B) =: C.
+   \f]
+
+   Letting \f$t^\prime:=a_i+t-1\f$, we are thus looking for a
+   \f$t^\prime\f$ with \f$a_i\leq t^\prime<B\f$ such that
+   \f$d_i(t^\prime)\leq C\f$. To make the inner slice as much simpler
+   as possible, we would like to find the largest such t, if
+   any. TermGrader has a method that does this.
+
+   The other way of getting a non-improving outer slice is with a
+   negative grading sign and a fake power. The fake power allows to
+   set the value at the i'th entry down to the value of zero, which is
+   better than any other exponent when the sign is negative.
+
+   A non-trivial outer slice will not have the fake power, and thus
+   possibly decrease the upper bound of the outer slice relative to
+   the current slice, which may make it non-improving. This is most
+   likely to happen for the largest possible \f$t=b_i\f$, and we want
+   \f$t\f$ to be large anyway in order to make the inner slice as
+   simple as possible, so it only makes sense to check this for
+   \f$t=b_i-a_i\f$. As the sign is negative, we get that \f[
+     d(a,b^\prime)=d(a,b)-d_i(b_i)+d_i(a_i),
+   \f] where we are noting that \f$d_i(b_i)=d_i(0)\f$. The question is then
+   whether \f$d(a,b^\prime)\leq\max\f$.
+
+   @param divisor The divisor \f$a\f$ associated to the slice.
+
+   @param dominator The dominator \f$b\f$ associated to the slice.
+
+   @param degree The upper bound \f$d(a,b)\f$ associated to the slice
+   (passed as a parameter to avoid recomputation).
+
+   @param pivot Is set to a pivot that generates a non-improving
+    outer slice if one is found.
+
+   @return Returns true if and only if a non-improving outer slice was
+    found.
+  */
   bool getInnerSimplify
 	(const Term& divisor,
 	 const Term& dominator,
 	 const mpz_class& degree,
 	 Term& pivot);
+
+  /** Find an inner slice that is non-improving, allowing us to
+   replace the current slice with the outer slice. We only need to know a
+   divisor \f$a\f$ and dominator \f$b\f$ of the current slice to perform this
+   analysis. If \f$a_i=b_i\f$ then there is nothing to be done for the
+   \f$i\f$'th entry, so assume that \f$a_i<b_i\f$.
+
+   There are two ways to obtain a non-improving inner slice. The first
+   is if the grading sign is negative, there is no fake power of
+   \f$x_i\f$, and \f$d(a^\prime,b)\leq \max\f$. Since the sign is
+   negative and there is no fake power, we have that \f[
+     d(a^\prime,b) = d(a,b) - d_i(a_i) + d_i(a_i+t),
+   \f], whereby \f$d(a^\prime,b)\leq \max\f$ is equivalent to \f[
+     d_i(a_i+t) \leq \max - d(a,b) + d_i(a_i) =: C.
+   \f]
+
+   Letting \f$t^\prime:=a_i+t\f$, we are thus looking for a
+   \f$t^\prime\f$ with \f$a_i< t^\prime\leq b_i^\prime\f$ such that
+   \f$d_i(t^\prime)\leq C\f$. To make the outer slice as much simpler
+   as possible, we would like to find the smallest such t, if
+   any. TermGrader has a method that does this.
+
+   Note that the assumption above that there is no fake power of
+   \f$x_i\f$ does not rule out any useful simplification, as in that
+   case the inner slice will never be non-improving, since it would
+   also have the fake power (in our analysis of just looking at
+   \f$a\f$ and \f$b\f$), so that the upper bound of the inner slice
+   would be equal to the upper bound of the current slice.
+
+   The other way of getting a non-improving inner slice is with a
+   positive grading sign and a fake power. If \f$t\f$ is as large as
+   possible, i.e. \f$t=b_i-a_i\f$, then the outer slice is forced to
+   use the fake power, which decreases the \f$i\f$'th entry down to
+   the value for zero, which is the worst possible when the sign is
+   positive, and this might make the inner slice non-improving.
+
+   As the sign is positive, and \f$b_i\f$ is a fake exponent, we get
+   that \f[
+     d(a^\prime,b)=d(a,b)-d_i(b_i-1)+d_i(b_i):=C,
+   \f] and it is then a question of whether \f$C\leq\max\f$.
+
+   @param divisor The divisor \f$a\f$ associated to the slice.
+
+   @param dominator The dominator \f$b\f$ associated to the slice.
+
+   @param degree The upper bound \f$d(a,b)\f$ associated to the slice
+    (passed as a parameter to avoid recomputation).
+
+   @param pivot Is set to a pivot that generates a non-improving
+    inner slice if one is found.
+
+   @return Returns true if and only if a non-improving inner slice was
+    found.
+  */
   bool getOuterSimplify
 	(const Term& divisor,
 	 const Term& dominator,
 	 const mpz_class& degree,
 	 Term& pivot);
 
-
   /** Sets dominator to be a term dominating every element of the
    content of slice.
 
-   Returns false (and clears slice) only if slice is a base case
-   slice, but may return true even if it is a base case slice.
+   Returns false (and clears slice) only if slice is a trivial base
+   case slice, but may return true even if it is a base case slice.
   */
   bool getDominator(Slice& slice, Term& dominator);
 
