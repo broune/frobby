53e36f3364599cbc01cdf10065e7e7482e69f711
Bjarke Hammersholt Roune <www.broune.com@localhost>
2010-02-09 04:40:00 +0100

    _

--- src/LatticeAnalyzeAction.cpp
+++ src/LatticeAnalyzeAction.cpp
@@ -29,6 +29,66 @@
 #include "DebugStrategy.h"
 
 #include <algorithm>
+#include <set>
+#include <sstream>
+
+namespace {
+  void printTriangles(const SatBinomIdeal& ideal) {
+	map<string, string> tris;
+	set<string> sums;
+	vector<mpz_class> sum(ideal.getVarCount());
+
+	size_t count = 0;
+	for (size_t gen1 = 0; gen1 < ideal.getGeneratorCount(); ++gen1) {
+	  for (size_t gen2 = gen1 + 1; gen2 < ideal.getGeneratorCount(); ++gen2) {
+		const vector<mpz_class>& g1 = ideal.getGenerator(gen1);
+		const vector<mpz_class>& g2 = ideal.getGenerator(gen2);
+
+		// Set sum = g1 + g2.
+		for (size_t var = 0; var < ideal.getVarCount(); ++var)
+		  sum[var] = g1[var] + g2[var];
+
+		ostringstream sumStr;
+		sumStr << "sum:";
+		for (size_t var = 0; var < ideal.getVarCount(); ++var)
+		  sumStr << ' ' << sum[var];
+		sumStr << '\n';
+		sums.insert(sumStr.str());
+		if (ideal.isPointFreeBody(g1, sum) && ideal.isPointFreeBody(g2, sum)) {
+		  ostringstream tmp;
+		  tmp << tris[sumStr.str()] << " g" << gen1 << " and g" << gen2 << '\n';
+		  tris[sumStr.str()] = tmp.str();
+		  ++count;
+		}
+	  }
+	}
+
+	if (ideal.hasZeroEntry())
+	  cout << "The ideal is not generic.\n";
+	else
+	  cout << "The ideal is generic.\n";
+	size_t sumCount =
+	  (ideal.getGeneratorCount() * (ideal.getGeneratorCount() - 1)) / 2;
+	cout << "There are " << sums.size() << " distinct sums out of "
+		 << sumCount << ".\n";
+	cout << "There are " << tris.size() << " distinct double triangle sums.\n";\
+	cout << "There are " << count << " double triangles.\n\n";
+
+	cout << "The " << ideal.getGeneratorCount() << " neighbors are:\n";
+	for (size_t g = 0; g < ideal.getGeneratorCount(); ++g) {
+	  cout << " g" << g << ": ";
+	  for (size_t var = 0; var < ideal.getVarCount(); ++var)
+		cout << '\t' << ideal.getGenerator(g)[var];
+	  cout << '\n';
+	}
+
+	cout << "\n\nThe " << count << " double triangles, grouped by sum:\n\n";
+
+	for (map<string, string>::iterator it = tris.begin();
+		 it != tris.end(); ++it)
+	  cout << it->first << it->second << '\n';
+  }
+}
 
 LatticeAnalyzeAction::LatticeAnalyzeAction():
   Action
@@ -70,6 +130,8 @@ void LatticeAnalyzeAction::perform() {
     fputs("generic.\n", stdout);
   else
     fputs("not generic.\n", stdout);
+
+  printTriangles(ideal);
 
   if (triCount == 0)
     exit(1);
