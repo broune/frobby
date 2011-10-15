ca3dbee5b1f58f7841f57ebdc633b9a17e0e26fd
Bjarke Hammersholt Roune <www.broune.com@localhost>
2011-10-15 15:42:12 -0400

    moved buffer pool to use MemoryBlocks.

--- src/BufferPool.cpp
+++ src/BufferPool.cpp
@@ -18,27 +18,38 @@
 #include "stdinc.h"
 #include "BufferPool.h"
 
+#include <utility>
+#include <limits>
+
 BufferPool::BufferPool(size_t bufferSize):
-_bufferSize(bufferSize > sizeof(FreeNode) ? bufferSize : sizeof(FreeNode)),
-_free(0) {
+_bufferSize(MemoryBlocks::alignThrowOnOverflow(std::max(bufferSize, sizeof(FreeNode)))),
+_free(0),
+_blocks() {
 }
 
-void* BufferPool::alloc() {
-  if (_free != 0) {
-    void* ptr = _free;
-	_free = _free->next;
-	return ptr;
-  } else
-	return _arena.alloc(_bufferSize);
-}
+void BufferPool::growCapacity() {
+  // ** Calcuate size of block (doubles capacity)
+  size_t size = block().getBytesInBlock();
+  if (size == 0) {
+    // start out at 10 buffers
+    ASSERT(block.isNull());
+    if (_bufferSize > std::numeric_limits<size_t>::max() / 10)
+      throw bad_alloc(); // _bufferSize * 10 overflows
+    size = _bufferSize * 10;
+  } else {
+    // double the size
+    if (size > std::numeric_limits<size_t>::max() / 2)
+      throw bad_alloc(); // size * 2 overflows
+    size *= 2;
+  }
 
-void BufferPool::free(void* ptr) {
-  FreeNode* node = reinterpret_cast<FreeNode*>(ptr);
-  node->next = _free;
-  _free = node;
+  // ** Allocate next block
+  ASSERT(MemoryBlocks::alignNoOverflow(size) == size);
+  ASSERT(size > block().getBytesInBlock());
+  _blocks.allocBlock(size);
 }
 
 void BufferPool::clear() {
   _free = 0;
-  _arena.clear();
+  _blocks.freeAllPreviousBlocks();
 }
